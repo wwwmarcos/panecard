6ce1d282fc84e1271c229a3aad11e563f8b9d32a
dstepbw <dstepbw@gmail.com>
2017-06-17 21:00:46 -0300

    update PanelBody tests

--- test/PanelBody.spec.js
+++ test/PanelBody.spec.js
@@ -3,10 +3,18 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import PanelBody from '../src/PanelBody'
 
+const PANEL_BODY = <PanelBody content='Lorem ipsum' />
+const PANEL_BODY_CLASS = 'react-panel-body'
+
 test('Render PanelBody component', t => {
   const expected = (
     <div className='react-panel-body'>Lorem ipsum</div>
   )
-  const wrapper = shallow(<PanelBody content='Lorem ipsum' />)
+  const wrapper = shallow(PANEL_BODY)
   t.true(wrapper.contains(expected))
+})
+
+test('Has react-panel-body class', t => {
+  const wrapper = shallow(PANEL_BODY)
+  t.true(wrapper.hasClass(PANEL_BODY_CLASS))
 })
